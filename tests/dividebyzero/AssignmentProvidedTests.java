6de70fa575aa7a4fd1439322d74133a9f5521a01
Anjali Pal <anjalip@cs.washington.edu>
2025-01-27 14:43:52 -0800

    tests

--- tests/dividebyzero/AssignmentProvidedTests.java
+++ tests/dividebyzero/AssignmentProvidedTests.java
@@ -87,4 +87,69 @@ class AssignmentProvidedTests {
     // :: error: divide.by.zero
     int k = 1 / z;
   }
+
+  public static void a() {
+    int one = 1;
+    int zero = 0;
+    // :: error: divide.by.zero
+    int a = one / zero;
+    // :: error: divide.by.zero
+    int b = one % zero;
+
+    int c = 5;
+    // :: error: divide.by.zero
+    c /= zero;
+    int d = 10;
+    // :: error: divide.by.zero
+    d %= zero;
+  }
+
+  public static void b() {
+    int a = 2 + 2;
+    int b = 0 / a;
+
+    int c = a * 10;
+    int d = b / c;
+
+    int e = -2 + -5;
+    int f = -4 / e;
+
+    int g = -10 * -1000;
+    int h = 8 / g;
+
+    int i = 7 * -4;
+    int j = -2 / h;
+  }
+
+  public static void c() {
+    int a = 2 + 2;
+    int b = 0 / a;
+
+    int c = a * 10;
+    int d = b % c;
+
+    int e = -2 + -5;
+    int f = -4 % e;
+
+    int g = -10 * -1000;
+    int h = 8 % g;
+
+    int i = 7 * -4;
+    // :: error: divide.by.zero
+    int j = -2 % h;
+  }
+
+  public static void d() {
+    // :: error: divide.by.zero
+    int a = (1 + 2 / 3) / (4 - 5);
+
+    int b = (1 + 2 / 3) / (4 - -5);
+
+    int c = 5 % (1 + 3);
+    // :: error: divide.by.zero
+    int d = 10 / c;
+
+    // :: error: divide.by.zero
+    int e = -2 / (1 + 2 * -3);
+  }
 }
